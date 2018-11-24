2576b3881f919c9217d5ec292fe79a3f257f7c52
SalmanFahad <44467020+SalmanFahad@users.noreply.github.com>
2018-11-24 22:40:43 +0300

    Add files via upload

---
+++ Sharing-BOT.js
@@ -0,0 +1,15 @@
+﻿const Discord = require('discord.js');
+const SALMAN = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+SALMAN.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+إجعل لنا من لُقياك نصيب:black_heart::sparkles:..
+ [ http://discord.gg/mVmkTNH ] 
+**`) 
+}).catch(console.error)
+})
+SALMAN.login('NTE1ODUzMjkyODAzNDU3MDM0.Dts2cg.zMyBNSFMB0NonE2I749yEEPOjwM');
